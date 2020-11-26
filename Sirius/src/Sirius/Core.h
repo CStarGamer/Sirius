db0a83ab01a48c90f8ba1bf68daa3f7b22ea611d
CStarGamer <cstargamer14@gmail.com>
2020-11-25 22:00:03 -0500

    Update .gitignore

--- Sirius/src/Sirius/Core.h
+++ Sirius/src/Sirius/Core.h
@@ -2,9 +2,9 @@
 
 #ifdef SR_PLATFORM_WINDOWS
 	#ifdef SR_BUILD_DLL
-	#define SIRIUS_API __declspec(dllexport)
+		#define SIRIUS_API __declspec(dllexport)
 	#else
-	#define SIRIUS_API __declspec(dllimport)
+		#define SIRIUS_API __declspec(dllimport)
 	#endif // SR_BUILD_DLL
 #else
 	#error Sirius currently only supports Windows!
