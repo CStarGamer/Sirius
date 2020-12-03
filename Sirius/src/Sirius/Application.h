74c7f812b695f04a445359f6bdfe6d002bf0f750
CStarGamer <cstargamer14@gmail.com>
2020-12-03 00:52:49 -0500

    Added OpenGL buffer abstraction

--- Sirius/src/Sirius/Application.h
+++ Sirius/src/Sirius/Application.h
@@ -10,6 +10,7 @@
 #include "Sirius/ImGui/ImGuiLayer.h"
 
 #include "Sirius/Renderer/Shader.h"
+#include "Sirius/Renderer/Buffer.h"
 
 namespace Sirius {
 	class Application
@@ -35,8 +36,10 @@ namespace Sirius {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 		static Application* s_Instance;
 	};
---
+++ Sirius/src/Sirius/Renderer/Renderer.cpp
@@ -0,0 +1,6 @@
+#include "srpch.h"
+#include "Renderer.h"
+
+namespace Sirius {
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
