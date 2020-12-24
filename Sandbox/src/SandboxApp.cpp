814ae51624cc6d9d5ed0c140fafd467fe71253df
CStarGamer <cstargamer14@gmail.com>
2020-12-23 23:50:06 -0500

    Moving some rendering code into Sandbox. Added camera movement and rotation, and resetting it.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -5,35 +5,188 @@
 class ExampleLayer : public Sirius::Layer {
 public:
 	ExampleLayer()
-		: Layer("Example")
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
 	{
-		
+		m_VertexArray.reset(Sirius::VertexArray::Create());
+
+		float vertices[3 * 7] = { // X, Y, Z
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 0.0f,
+		};
+
+		std::shared_ptr<Sirius::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Sirius::VertexBuffer::Create(vertices, sizeof(vertices)));
+		Sirius::BufferLayout layout = {
+			{ Sirius::ShaderDataType::Float3, "a_Position" },
+			{ Sirius::ShaderDataType::Float4, "a_Color" }
+		};
+
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[3] = { 0, 1, 2 };
+		std::shared_ptr<Sirius::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Sirius::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA.reset(Sirius::VertexArray::Create());
+
+		float squareVertices[3 * 4] = { // X, Y, Z
+			-0.75f, -0.75f, 0.0f,
+			0.75f, -0.75f, 0.0f,
+			0.75f, 0.75f, 0.0f,
+			-0.75f, 0.75f, 0.0f
+		};
+
+		std::shared_ptr<Sirius::VertexBuffer> squareVB;
+		squareVB.reset(Sirius::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB->SetLayout({
+			{ Sirius::ShaderDataType::Float3, "a_Position" }
+			});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndicies[6] = { 0, 1, 2, 2, 3, 0 };
+		std::shared_ptr<Sirius::IndexBuffer> squareIB;
+		squareIB.reset(Sirius::IndexBuffer::Create(squareIndicies, sizeof(squareIndicies) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+			
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+			
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(new Sirius::Shader(vertexSrc, fragmentSrc));
+
+		std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+			
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+			
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_BlueShader.reset(new Sirius::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 	}
 
 	void OnUpdate() override
 	{
-		//SR_INFO("ExapleLayer::Update");
+		if (Sirius::Input::IsKeyPressed(SR_KEY_LEFT))
+			m_CameraPosition.x -= m_CameraMoveSpeed;
+		else if (Sirius::Input::IsKeyPressed(SR_KEY_RIGHT))
+			m_CameraPosition.x += m_CameraMoveSpeed;
 
-		if (Sirius::Input::IsKeyPressed(SR_KEY_TAB))
-			SR_TRACE("Tab is pressed!");
+		if (Sirius::Input::IsKeyPressed(SR_KEY_UP))
+			m_CameraPosition.y += m_CameraMoveSpeed;
+		else if (Sirius::Input::IsKeyPressed(SR_KEY_DOWN))
+			m_CameraPosition.y -= m_CameraMoveSpeed;
+		
+		if (Sirius::Input::IsKeyPressed(SR_KEY_A))
+			m_CameraRotation += m_CameraRotationSpeed;
+		else if (Sirius::Input::IsKeyPressed(SR_KEY_D))
+			m_CameraRotation -= m_CameraRotationSpeed;
+
+		Sirius::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		Sirius::RenderCommand::Clear();
+
+		m_Camera.SetPosition(m_CameraPosition);
+		m_Camera.SetRotation(m_CameraRotation);
+
+		Sirius::Renderer::BeginScene(m_Camera);
+
+		Sirius::Renderer::Submit(m_BlueShader, m_SquareVA);
+		Sirius::Renderer::Submit(m_Shader, m_VertexArray);
+
+		Sirius::Renderer::EndScene();
+
 	}
 
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Test");
-		ImGui::Text("Hello World!");
-		ImGui::End();
+		
 	}
 
 	void OnEvent(Sirius::Event& event) override
 	{
-		//SR_TRACE("{0}", event);
-		if (event.GetEventType() == Sirius::EventType::KeyPressed)
-		{
-			Sirius::KeyPressedEvent& e = (Sirius::KeyPressedEvent&)event;
-			SR_TRACE("{0}", (char)e.GetKeyCode());
-		}
+		Sirius::EventDispatcher dispatcher(event);
+		dispatcher.Dispatch<Sirius::KeyPressedEvent>(SR_BIND_EVENT_FN(ExampleLayer::OnKeyPressedEvent));
 	}
+
+	bool OnKeyPressedEvent(Sirius::KeyPressedEvent& event)
+	{
+		// Reset camera position and rotation.
+		if (event.GetKeyCode() == SR_KEY_R && event.GetRepeatCount() <= 0)
+		{
+			m_CameraPosition = { 0.0f, 0.0f, 0.0f };
+			m_CameraRotation = 0.0f;
+		}
+
+		return false;
+	}
+
+private:
+	std::shared_ptr<Sirius::Shader> m_Shader;
+	std::shared_ptr<Sirius::VertexArray> m_VertexArray;
+
+	std::shared_ptr<Sirius::Shader> m_BlueShader;
+	std::shared_ptr<Sirius::VertexArray> m_SquareVA;
+
+	Sirius::OrthographicCamera m_Camera;
+	glm::vec3 m_CameraPosition;
+	float m_CameraMoveSpeed = 0.1f;
+	float m_CameraRotation = 0.0f;
+	float m_CameraRotationSpeed = 2.0f;
 };
 
 class Sandbox : public Sirius::Application {
--- Sirius/src/Sirius/Application.h
+++ Sirius/src/Sirius/Application.h
@@ -39,14 +39,6 @@ namespace Sirius {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SquareVA;
-
-		OrthographicCamera m_Camera;
-
 		static Application* s_Instance;
 	};
 
