e47fdc1530163df729f17ed2bcb83b2ed902bd47
CStarGamer <cstargamer14@gmail.com>
2020-12-31 14:10:06 -0500

    Implemented shader abstraction and uniforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,7 +2,10 @@
 
 #include "imgui/imgui.h"
 
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Platform/OpenGL/OpenGLShader.h"
 
 class ExampleLayer : public Sirius::Layer {
 public:
@@ -88,9 +91,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Sirius::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Sirius::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -107,21 +110,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
-			in vec4 v_Color;
+			
+			uniform vec3 u_Color;
 			
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Sirius::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Sirius::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Sirius::Timestep ts) override
@@ -151,14 +155,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Sirius::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Sirius::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 10; y++)
 		{
 			for (int x = 0; x < 10; x++)
 			{
 				glm::vec3 pos(x * 0.2f, y * 0.2f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-
-				Sirius::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Sirius::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 			
 		}
@@ -170,7 +176,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Sirius::Event& event) override
@@ -195,7 +203,7 @@ private:
 	std::shared_ptr<Sirius::Shader> m_Shader;
 	std::shared_ptr<Sirius::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Sirius::Shader> m_BlueShader;
+	std::shared_ptr<Sirius::Shader> m_FlatColorShader;
 	std::shared_ptr<Sirius::VertexArray> m_SquareVA;
 
 	Sirius::OrthographicCamera m_Camera;
@@ -203,6 +211,8 @@ private:
 	float m_CameraMoveSpeed = 1.0f;
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Sirius::Application {
--- Sirius/src/Sirius/Renderer/Renderer.cpp
+++ Sirius/src/Sirius/Renderer/Renderer.cpp
@@ -1,6 +1,7 @@
 #include "srpch.h"
 #include "Renderer.h"
-#include "RenderCommand.h"
+
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Sirius {
 
@@ -17,8 +18,8 @@ namespace Sirius {
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
