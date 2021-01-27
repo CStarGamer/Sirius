169d3d155db9bce5dcba15480f81ca60cd2f946c
CStarGamer <cstargamer14@gmail.com>
2021-01-27 12:15:57 -0500

    Optimized 2D renderer to use only one single shader

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -34,7 +34,7 @@ void Sandbox2D::OnUpdate(Sirius::Timestep ts)
 
 	Sirius::Renderer2D::DrawQuad({ 0.5f, 0.5f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 	Sirius::Renderer2D::DrawQuad({ 0.0f, 0.2f }, { 0.75f, 0.5f }, { 0.65f, 0.96f, 1.0f, 1.0f });
-	Sirius::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 2.0f, 2.0f }, m_CheckerboardTexture);
+	Sirius::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
 
 	Sirius::Renderer2D::EndScene();
 	
--- Sirius/src/Sirius/Renderer/Texture.cpp
+++ Sirius/src/Sirius/Renderer/Texture.cpp
@@ -5,6 +5,20 @@
 #include "Platform/OpenGL/OpenGLTexture.h"
 
 namespace Sirius {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			SR_CORE_ASSERT(false, "RendererAPI::None is currently not supported.");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		default:
+			SR_CORE_ASSERT(false, "Unknown RendererAPI!");
+			return nullptr;
+		}
+	}
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
@@ -13,7 +27,7 @@ namespace Sirius {
 			SR_CORE_ASSERT(false, "RendererAPI::None is currently not supported.");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		default:
 			SR_CORE_ASSERT(false, "Unknown RendererAPI!");
 			return nullptr;
